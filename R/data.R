2617915a694d1ddef17dd9185c9dae485a1ab8cf
Alfredo Hernández <aldomann.designs@gmail.com>
2019-07-12 19:19:11 +0200

    Split .R files for roxygen2 documentation into mariokart (package) & data

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' "Mario Kart 8: Characters"
+#'
+#' A data set containing character statistics for Mario Kart 8.
+#'
+#' @format A data frame with 45 observations of 14 variables
+"mk8_characters"
+
+#' "Mario Kart 8: Vehicles"
+#'
+#' A data set containing vehicle statistics for Mario Kart 8.
+#'
+#' @format A data frame with 40 observations of 15 variables
+"mk8_vehicles"
+
+#' "Mario Kart 8: Wheels"
+#'
+#' A data set containing wheels statistics for Mario Kart 8.
+#'
+#' @format A data frame with 21 observations of 13 variables
+"mk8_wheels"
+
+#' "Mario Kart 8: Gliders"
+#'
+#' A data set containing glider statistics for Mario Kart 8.
+#'
+#' @format A data frame with 14 observations of 13 variables
+"mk8_gliders"
---
+++ R/mariokart.R
@@ -0,0 +1,11 @@
+#' Mario Kart Statistics from Different Games
+#'
+#' This package contains complete statistics for Mario Kart games
+#' @docType package
+#' @name mariokart
+#'
+#' @examples
+#' data(mk8_characters)
+#'
+#' plot(data = mk8_characters, speed_normal ~ handling_normal)
+NULL
